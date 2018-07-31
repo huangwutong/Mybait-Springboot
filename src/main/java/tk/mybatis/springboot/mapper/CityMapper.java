5b5dd4e02e395e6900fabb2d0dfb17c0f5d49cf5
Your Name <huangwutongdd@163.com>
2018-07-31 21:08:57 +0800

    first commit

---
+++ src/main/java/tk/mybatis/springboot/mapper/CityMapper.java
@@ -0,0 +1,9 @@
+
+package tk.mybatis.springboot.mapper;
+
+import tk.mybatis.springboot.model.City;
+import tk.mybatis.springboot.util.MyMapper;
+
+
+public interface CityMapper extends MyMapper<City> {
+}
---
+++ src/main/java/tk/mybatis/springboot/model/BaseEntity.java
@@ -0,0 +1,43 @@
+
+
+package tk.mybatis.springboot.model;
+
+import javax.persistence.*;
+
+
+public class BaseEntity {
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    //通过这个可以添加表中不存在的字段
+    @Transient
+    private Integer page = 1;
+
+    @Transient
+    private Integer rows = 10;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+}
